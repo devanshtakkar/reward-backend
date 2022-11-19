ed880486dae49b040c3fd0f8daf3a6b059a385eb
devanshtakkar <devansh@takkar.ooo>
2022-11-19 14:41:30 +0530

    allowed all cors requests

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors"
+import cors from "cors";
 import "dotenv/config";
 import adminRoutes from "./admin/admin.js";
 import userRoutes from "./user/users.js";
-import fileUpload from 'express-fileupload';
+import fileUpload from "express-fileupload";
 import OtpAutoExpiry from "./utils/otp-auto-expiry.js";
 
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(
-	cors({
-		origin: ["https://hoppscotch.io", "http://localhost:3001", "https://app.ezeeroll.in"],
-		credentials: true,
-	})
-);
-
+app.use(cors());
 
 //body parsing middlewares
 app.use(express.json());
 app.use(express.text());
-app.use(fileUpload({
-	limits: {
-		fileSize: 2000000,
-	},
-	abortOnLimit: true
-}));
-
+app.use(
+	fileUpload({
+		limits: {
+			fileSize: 2000000,
+		},
+		abortOnLimit: true,
+	})
+);
 
 //serving static files
-app.use(express.static("static"))
+app.use(express.static("static"));
 
 //view engine setting
 app.set("views", "./views");
-app.set("view engine", "pug")
+app.set("view engine", "pug");
 
 //routes
 app.use("/admin", adminRoutes);
@@ -44,8 +39,8 @@ app.use("/static", express.static("files"));
 // app.use("/test",test)
 
 app.listen(3001, () => {
-    console.log("Server started on port 3001")
-})
+	console.log("Server started on port 3001");
+});
 
 OtpAutoExpiry();
-export {prisma}
+export { prisma };
